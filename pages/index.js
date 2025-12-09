6fcf9676d0c7642c565adf4425d1694cc7ae0f20
Marcelo Hippolyto de Sandes Peixoto <marcelo052001@gmail.com>
2025-12-09 20:15:37 +0000

    mudança da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>Fátima, te amo!</h1>;
+  return (
+    <>
+      <h1>Site em construção.</h1>
+      <h2>
+        Criação do melhor espaço para você aprender matemática e passar em
+        Medicina!
+      </h2>
+    </>
+  );
 }
 
 export default Home;
